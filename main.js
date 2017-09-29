491f48c8132e01fa50140842ce133317ff87bcfe
Jashua R. White <jashuawhite@gmail.com>
2017-09-28 23:35:54 -0400

    Setup toggle dropdown

--- main.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  $('.navigation__menu-toggle ').on('click', function() {
-    $('.navigation__nav-section').stop(true,true).toggleClass('expanded'); // stop() prevents chained click animations
-  });
-});
---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $('.js-nav-toggle').on('click', function(e) {
+    e.preventDefault();
+    $(this).toggleClass('active');
+    $('.navigation').stop(true, true).slideToggle("slow", function() {
+      $(this).toggleClass('expanded');
+    });
+  });
+  $(window).resize(function() {
+    var $navigationEl = $('.navigation');
+    var windowWidth = $(window).width();
+    if(windowWidth >= "768") {
+      $navigationEl.show();
+    } else if (windowWidth < "768" && !$navigationEl.hasClass('expanded')) {
+      // If the menu was expanded
+      $navigationEl.hide();
+    }
+  });
+});
